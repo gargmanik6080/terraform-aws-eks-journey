1015b755b36ac15b392fee46b5d485510720106b
gargmanik6080 <gargmanik6080@gmail.com>
2025-06-02 17:25:58 +0530

    setting up ingress

--- 6. Deploy pods in cluster created by eksctl pt 2/setup.sh
+++ 6. Deploy pods in cluster created by eksctl pt 2/setup.sh
@@ -1,3 +1,5 @@
+set -eu
+
 eksctl create cluster \
   --name my-test-cluster \
   --region us-west-2 \
@@ -16,6 +18,33 @@ kubectl apply -f frontend-ingress.yaml
 
 
 aws ec2 describe-instances \
-  --filters "Name=private-ip-address,Values=192.168.73.68" \
+  --filters "Name=private-ip-address,Values=192.168.70.170" \
   --query "Reservations[*].Instances[*].PublicDnsName" \
   --output text
+
+
+# eksctl utils associate-iam-oidc-provider \
+#   --region us-west-2 \
+#   --cluster my-test-cluster \
+#   --approve
+
+
+# eksctl create iamserviceaccount \
+#     --cluster=my-test-cluster \
+#     --namespace=kube-system \
+#     --name=aws-load-balancer-controller \
+#     --attach-policy-arn=arn:aws:iam::997489286992:policy/AWSLoadBalancerControllerIAMPolicy \
+#     --override-existing-serviceaccounts \
+#     --region us-west-2 \
+#     --approve
+
+helm repo add eks https://aws.github.io/eks-charts
+helm repo update eks
+
+
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+  -n kube-system \
+  --set clusterName=my-test-cluster \
+  --set serviceAccount.create=true \
+  --set serviceAccount.name=aws-load-balancer-controller \
+  --version 1.13.0
